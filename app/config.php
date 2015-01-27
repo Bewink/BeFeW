d9df2a5655648b43aa076f60f39cfa0d467f0abe
Yann <yannbergonzat@gmail.com>
2015-01-28 00:23:51 +0100

    BeFeW v0.6.2 - Improved error reporting

--- app/config.php
+++ app/config.php
@@ -8,11 +8,11 @@ define('TEMPLATES_FOLDER', 'twig');
 define('SCRIPTS_FOLDER', 'js');
 
 if(DEBUG) {
-    error_reporting(E_ALL);
+    error_reporting(-1);
     ini_set('display_errors', 1);
     $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } else {
-    error_reporting(E_STRICT);
+    error_reporting(0);
     ini_set('display_errors', 0);
     $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 }
